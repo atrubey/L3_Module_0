77c93b42f771f02af3cea0c5f39d5f0cc463686b
league <league@wts-imac-11.attlocal.net>
2017-12-03 10:49:53 -0800

    Finished IntroToHashMaps

--- src/IntroToHashMaps/IntroToHashMaps.java
+++ src/IntroToHashMaps/IntroToHashMaps.java
@@ -3,12 +3,15 @@ package IntroToHashMaps;
 import java.util.HashMap;
 
 public class IntroToHashMaps {
+	public static void main(String[] args) {
 	//1. Create a HashMap called roots with Integers for the keys and Doubles for the values.
 	HashMap<Integer, Double> roots = new HashMap<Integer, Double>();
 	
 	//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
 	//   current iteration of the loop (i). The value entry will be the square root of i.
-	
+	for(int i = 0; i<500; i++) {
+		roots.put(i, Math.sqrt(i));
+	}
 	
 	//3. Iterate through all the entries in your HashMap displaying the keys with their respective
 	//   square roots (values). Use the following format.
@@ -17,4 +20,10 @@ public class IntroToHashMaps {
 	//   The square Root of 1 is 1.0
 	//   The square Root of 2 is 1.4142135623730951
 	//   The square Root of 3 is 1.7320508075688772
+	
+	for(Integer i : roots.keySet()) {
+		System.out.println("The square Root of " + i + " is " + roots.get(i));
+	}
+	
+	}
 }
--- src/IntroToHashMaps/LogSearch.java
+++ src/IntroToHashMaps/LogSearch.java
@@ -1,6 +1,17 @@
 package IntroToHashMaps;
 
-public class LogSearch {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+import IntroToArrayLists.GuestBook;
+
+public class LogSearch implements ActionListener {
   /* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
@@ -28,4 +39,90 @@ public class LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+	
+	JFrame frame = new JFrame(); 
+	JPanel panel = new JPanel();
+	JButton button1 = new JButton("Add Entry"); 
+	JButton button2 = new JButton("Search by ID");
+	JButton button3 = new JButton("View List"); 
+	JButton button4 = new JButton("Remove Entry");
+	HashMap<Integer, String> idNumbers = new HashMap<Integer, String>();
+	
+	public static void main(String[] args) {
+		
+		LogSearch log = new LogSearch(); 
+		log.createUI();
+	
+	}
+	
+	void createUI() {
+		
+		frame.add(panel);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		
+		frame.setVisible(true);
+		frame.setSize(600, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		button4.addActionListener(this);
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(button1)) {
+			String id = JOptionPane.showInputDialog("Add an ID number: ");
+			String n = JOptionPane.showInputDialog("Add a name: ");
+			
+			int idInt = Integer.parseInt(id);
+			
+			idNumbers.put(idInt, n);
+			
+		}
+
+		if(e.getSource().equals(button2)) {
+			String id = JOptionPane.showInputDialog("Search for an ID number: ");
+			int idInt = Integer.parseInt(id);
+			
+			if (idNumbers.get(idInt) != null) {
+				JOptionPane.showMessageDialog(null,"ID: " + idInt + " Name: " + idNumbers.get(idInt));
+			} else {
+				JOptionPane.showMessageDialog(null,"There is no entry for ID " + idInt + ". ");
+			}
+			
+		}	
+		
+		if(e.getSource().equals(button3)) {
+			
+			String fullList = "";
+			
+			for(Integer i : idNumbers.keySet()) {
+				fullList += "ID: " + i + " Name: " + idNumbers.get(i) + "\n";
+			}
+			
+			JOptionPane.showMessageDialog(null, fullList);
+			
+		}
+		
+		if(e.getSource().equals(button4)) {
+			String id = JOptionPane.showInputDialog("Remove an entry with ID: ");
+			int idInt = Integer.parseInt(id);
+			
+			if (idNumbers.get(idInt) != null) {
+				idNumbers.remove(idInt); 
+			} else {
+				JOptionPane.showMessageDialog(null,"There is no entry for ID " + idInt + ". ");
+			}
+			
+		}
+		
+	}
+	
+	
 }
